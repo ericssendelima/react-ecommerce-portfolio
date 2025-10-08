61c294695220b088bdac8019fda791597d3ecc0a
ericssendelima <ericssendelima@gmail.com>
2025-10-08 07:49:46 -0300

    feat(routing): set up basic navigation with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './styles/App.module.css'
+import { Routes, Route } from 'react-router'
+
+//Pages
+import { ProductListingPage } from './features/product-listing'
+import { ShoppingCartPage } from './features/shopping-cart'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<ProductListingPage />} />
+      <Route path="/cart" element={<ShoppingCartPage />} />
+    </Routes>
   )
 }
 
