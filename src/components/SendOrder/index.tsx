fa28d0cab80073dcb77c4e2d8589816c4274a14a
ericssendelima <ericssendelima@gmail.com>
2025-10-09 11:04:13 -0300

    feat(cart): 🚀 implement the logic  sending to WhatsApp with the order summary

--- src/components/SendOrder/index.tsx
+++ src/components/SendOrder/index.tsx
@@ -1,21 +1,28 @@
-import { FaWhatsapp } from "react-icons/fa";
+import { useCart } from "../../hooks/useCart";
 import styles from "./SendOrder.module.css";
 
+import { FaWhatsapp } from "react-icons/fa";
+
 export const SendOrder = () => {
+  const { cartProducts } = useCart();
+  const totalTeste = cartProducts.reduce((acc, product) => {
+    return acc + (product.prodTotal * product.quantidade);
+  }, 0);
+
   let texto = `PEDIDO`;
 
-  // context.map((obj) => {
-  //   texto += `
-  //  ${obj.name}
-  //  -  quantidade: ${obj.quantidadeEpi}
-  //  -  valor: R$ ${obj.prodTotal.toFixed(2)}
+  cartProducts.map((obj) => {
+    texto += `
+   ${obj.name}
+   -  quantidade: ${obj.quantidade}
+   -  valor: R$ ${obj.prodTotal.toFixed(2)}
 
-  //  `;
-  //   return obj;
-  // });
+   `;
+    return obj;
+  });
 
-  texto += `Valor total do pedido: `;
-  // R$ ${totalTeste.toFixed(2)}`;
+  texto += `Valor total do pedido: 
+  R$ ${totalTeste.toFixed(2)}`;
 
   function encode(texto: string) {
     return encodeURIComponent(texto).replace(/[!'()*]/g, function (c) {
